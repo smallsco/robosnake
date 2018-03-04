1fe7679fe0d8d6e8749c43ce05b519c1aa41f2b8
Scott Small <scott.small@rdbrck.com>
2018-03-04 05:33:12 +0000

    Gameday bugfix for starvation death conditions

--- algorithm.lua
+++ algorithm.lua
@@ -117,7 +117,7 @@ local function heuristic( grid, state, my_moves, enemy_moves )
         log( DEBUG, 'I am trapped.' )
         return -2147483648
     end
-    if state[ 'me' ][ 'health' ] < 0 then
+    if state[ 'me' ][ 'health' ] <= 0 then
         log( DEBUG, 'I am out of health.' )
         return -2147483648
     end
@@ -158,7 +158,7 @@ local function heuristic( grid, state, my_moves, enemy_moves )
         log( DEBUG, 'Enemy is trapped.' )
         score = score + 2147483647
     end
-    if state[ 'enemy' ][ 'health' ] < 0 then
+    if state[ 'enemy' ][ 'health' ] <= 0 then
         log( DEBUG, 'Enemy is out of health.' )
         score = score + 2147483647
     end
@@ -332,8 +332,8 @@ function algorithm.alphabeta( grid, state, depth, alpha, beta, alphaMove, betaMo
         
         -- short circuit win/loss conditions
         #moves == 0 or
-        state[ 'me' ][ 'health' ] < 0 or
-        state[ 'enemy' ][ 'health' ] < 0 or
+        state[ 'me' ][ 'health' ] <= 0 or
+        state[ 'enemy' ][ 'health' ] <= 0 or
         (
             state[ 'me' ][ 'body' ][ 'data' ][1][ 'x' ] == state[ 'enemy' ][ 'body' ][ 'data' ][1][ 'x' ]
             and state[ 'me' ][ 'body' ][ 'data' ][1][ 'y' ] == state[ 'enemy' ][ 'body' ][ 'data' ][1][ 'y' ]
