cb30394a158c8b448a70738c35aea23d18dd1e08
Scott Small <smallsco@gmail.com>
2019-02-26 20:41:00 -0800

    moar debug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 echo $PORT
+cat /etc/nginx/conf.d/server.conf
 sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/server.conf
+cat /etc/nginx/conf.d/server.conf
 exec /usr/local/openresty/bin/openresty -g "daemon off;"
