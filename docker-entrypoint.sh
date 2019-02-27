6daca7639e5f6992dc16c848c4389d1bc5e14e44
Scott Small <smallsco@gmail.com>
2019-02-26 20:54:31 -0800

    trying another tutorial method

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo $PORT
-cat /etc/nginx/conf.d/server.conf
-sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/server.conf
-cat /etc/nginx/conf.d/server.conf
+sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/server.conf
 exec /usr/local/openresty/bin/openresty -g "daemon off;"
